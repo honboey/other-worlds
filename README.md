2de2bf883eb9aadc7c18aca0e1ce0046180bb8a4
Hon <55781260+honboey@users.noreply.github.com>
2020-06-22 11:35:59 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,86 @@
+# Tailwind Starter Kit
+This is a repository that holds some common plugins, workflows and file structure that is useful for starting a Tailwind project. It uses Gulp as the toolkit to automate the workflow.
+
+## General workflow
+All source files are placed in [/src](/src). When you run `gulp develop` these files are processed and placed into [/dev](/dev). The files in [/dev](/dev) can be read by your browser.
+
+Out of the box, `gulp develop` does several things:
+* Watches any changes in [/src](/src) and processes these changes to [/dev](/dev)
+* Converts the original tailwind.css file to a browser-legible .css file.
+* Adds any necessary browser prefixes to the .css file so it is compatible on all browsers
+* Converts and collates the .njk files to browser-legible .html files
+* Creates folders based on these html file names and then renames them to index.html
+* Copies any javascript files
+* Copies any fonts
+* Processes all images to various sizes to enable lazy loading and responsive images
+
+When development has finished a `gulp production` command runs a series of scripts that processes all the [/dev](/dev) files and places them into [/public](/public). [/public](/public) is the final, production ready site. 
+
+`gulp production` does several things to get the files production ready:
+* Runs PurgeCSS to get rid on any unwanted CSS 
+* Minimises CSS to make it as lean as possible
+* Minimises and uglifies javascript files
+* Copies all fonts
+* Copies all images
+
+## Directory tree and file structure
+The directory tree looks like this
+```
+src (this holds all source files)
+|   |– img
+|   |– scripts
+|   |– styles
+|   |– templates (this holds .njk files which correspond to its .html page)
+|      |– partials (this holds the .html templating blocks)
+|   
+dev (this holds all development files)
+|   |– img
+|   |– scripts
+|    |– styles
+|    
+public (this is production ready code)
+|    |– img
+|    |– scripts
+|    |– styles 
+```
+
+## Geting started
+These instructions are for getting the project running on a local development environment.
+
+### System requirements
+* [NodeJS v12.16.3](https://nodejs.org)
+* [Gulp](https://gulpjs.com/)
+
+### Install plugins 
+Install all the necessary plug-ins by running `npm install` in the root of the project folder.
+
+### Run your first build
+`gulp develop`
+
+### Where to work
+You should always be working from [/src](/src).
+
+## Common commands
+
+### Development
+* `gulp development` processes and watches all [/src](/src) files and puts them into [/dev](/dev).
+
+   * `gulp css` processes .css files in [/dev](/dev).
+   * `gulp html` processes .njk files in [/src](/src) and converts them to .html and places them in [/dev](/dev).
+   * `gulp scripts` processes .js files in [/src](/src) and places them in [/dev](/dev).
+   * `gulp watchTask` watches any changes in [/src](/src) and runs the necessary script.
+
+### Production
+* `gulp production` processes and watches all [/dev](/dev) files and puts them into [/public](/public).
+
+   * `gulp cssProduction` processes .css files in [/dev](/dev).
+   * `gulp scriptsProduction` uglifies all .js files in [/dev](/dev)
+
+## Optional plug-ins
+There is another branch of this project called 'optionals'. This branch contains the following optional plugins:
+* [SwupJS](https://swup.js.org/) for animated page transitions
+* [gulp-responsive](https://www.npmjs.com/package/gulp-responsive) for making multiple resolutions of images
+* [LazySizes](https://github.com/aFarkas/lazysizes) for lazy loading images
+* [gulp-rename](https://www.npmjs.com/package/gulp-rename) for renaming files and directory paths. This is used for renaming html files to index.html and placing them in a folder named after the original filename.
+
+
---
+++ gulpfile.js
@@ -0,0 +1,253 @@
+const { src, dest, watch, series, parallel } = require("gulp");
+const rename = require("gulp-rename");
+const gulpIf = require("gulp-if");
+
+// css
+const postcss = require("gulp-postcss");
+const tailwind = require("tailwindcss");
+const autoprefixer = require("autoprefixer");
+const purgecss = require("gulp-purgecss");
+const cleanCss = require("gulp-clean-css");
+
+// html
+const nunjucks = require("gulp-nunjucks-render");
+
+// javscript
+const terser = require("gulp-terser");
+
+// images
+const responsive = require("gulp-responsive");
+const cache = require("gulp-cached");
+
+/******************************************************
+ * TASKS FOR DEVELOPMENT
+ * Processes all files in /src and places them in /dev
+ ******************************************************/
+
+// CSS processing – loads tailwind css file and adds browser compatibility prefixes
+function css() {
+  return src("src/styles/*.css")
+    .pipe(postcss([tailwind(), autoprefixer()]))
+    .pipe(dest("dev/styles"));
+}
+
+// HTML processing – compiles njk files and converts them to html
+function html() {
+  return (
+    src("src/templates/*.njk")
+      // Injects njk partials and then converts them to html files
+      .pipe(nunjucks({ path: "src/templates/partials" }))
+      // Runs gulp-rename on all files. Renames them index.html and puts them into a folder named after the file name. Excludes the original index.html and 404.html
+      .pipe(
+        gulpIf(
+          "!index.html",
+          gulpIf(
+            "!404.html",
+            // Creates a folder based on the file name and then renames the file index.html
+            rename(function (file) {
+              file.dirname = file.basename;
+              file.basename = "index";
+              file.extname = ".html";
+            })
+          )
+        )
+      )
+      .pipe(dest("dev"))
+  );
+}
+
+// Javascript processing – copies javascript files
+function scripts() {
+  return src("src/scripts/*.js").pipe(dest("dev/scripts"));
+}
+
+// Font processing – copies font files
+function typeface() {
+  return src("src/styles/*.woff").pipe(dest("dev/styles/"));
+}
+
+// Favicon processing – copies favicon files. Use https://realfavicongenerator.net to generate favicons
+function favicon() {
+  return src([
+    "src/favicon/*.png",
+    "src/favicon/*.svg",
+    "src/favicon/*.xml",
+    "src/favicon/*.ico",
+    "src/favicon/*.webmanifest",
+  ]).pipe(dest("dev"));
+}
+
+/****************
+ * Image processing
+ ****************/
+
+// Image resizing for responsive images – this ignores any image which has the suffix "-lazy.jpg"
+function images() {
+  return (
+    src(["src/img/**/*.{jpg,png}", "!src/img/**/*-lazy.{jpg,png}"])
+      // Only processes images which are new or changed
+      .pipe(cache("imageResizing"))
+      .pipe(
+        responsive(
+          {
+            "*": [
+              {
+                width: 800,
+                rename: { suffix: "-800w" },
+              },
+              {
+                width: 1080,
+                rename: { suffix: "-1080w" },
+              },
+              {
+                width: 1440,
+                rename: { suffix: "-1440w" },
+              },
+            ],
+            "**/*": [
+              {
+                width: 800,
+                rename: { suffix: "-800w" },
+              },
+              {
+                width: 1080,
+                rename: { suffix: "-1080w" },
+              },
+              {
+                width: 1440,
+                rename: { suffix: "-1440w" },
+              },
+            ],
+          },
+          {
+            quality: 85,
+            skipOnEnlargement: true,
+            errorOnEnlargement: false,
+          }
+        )
+      )
+      .pipe(dest("dev/img"))
+  );
+}
+
+// Copies over the original images to /dev
+function imageCopy() {
+  return src("src/img/**/*.{jpg,png,gif}").pipe(dest("dev/img"));
+}
+
+// Watchtasks
+function watchTask() {
+  watch(["src/styles/*.css", "tailwind.config.js"], parallel(css));
+  watch(["src/templates/**/*.njk"], parallel(html));
+  watch(["src/scripts/*.js"], parallel(scripts));
+  watch(["src/img/**/*.{jpg,png,gif}"], parallel(images, imageCopy));
+  watch(["src/favicon"], parallel(favicon));
+  watch(["src/styles/*.woff"], parallel(typeface));
+}
+
+/******************************************************
+ * TASKS FOR PRODUCTION
+ * Processes all files in /dev and places them in /pub
+ ******************************************************/
+
+// CSS processing – removes any unused CSS and uglifies / minimises
+function cssProduction() {
+  return (
+    src("dev/styles/*.css")
+      // purgeCSS removes any unsused CSS items
+      .pipe(
+        purgecss({
+          content: ["dev/**/*.html", "dev/scripts/*.js"],
+          defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+        })
+      )
+      // cleanCSS minifies CSS
+      .pipe(
+        cleanCss(
+          {
+            debug: true,
+          },
+          (details) => {
+            console.log(
+              details.name +
+                " was " +
+                details.stats.originalSize / 100 +
+                " bytes"
+            );
+            console.log(
+              details.name +
+                " is now " +
+                details.stats.minifiedSize / 100 +
+                " bytes"
+            );
+          }
+        )
+      )
+      .pipe(dest("public/styles"))
+  );
+}
+
+// HTML processing – copies over HTML files
+function htmlProduction() {
+  return src("dev/*.html").pipe(dest("public"));
+}
+
+// Javascript processing – uglifies / minimises javascript
+function scriptsProduction() {
+  return src("dev/scripts/*.js").pipe(terser()).pipe(dest("public/scripts"));
+}
+
+// Image processing – copies over images
+function imgProduction() {
+  return src("dev/img/**/*.{jpg,gif,png}").pipe(dest("public/img/"));
+}
+
+// Font processing – copies over fonts
+function typefaceProduction() {
+  return src("dev/styles/*.woff").pipe(dest("public/styles/"));
+}
+
+// Favicon processing - copies over favicon files
+function faviconProduction() {
+  return src([
+    "dev/*.png",
+    "dev/*.svg",
+    "dev/*.xml",
+    "dev/*.ico",
+    "dev/*.webmanifest",
+  ]).pipe(dest("public"));
+}
+
+/******************************************************
+ * TASKS
+ ******************************************************/
+
+exports.css = css;
+exports.html = html;
+exports.scripts = scripts;
+exports.images = images;
+exports.watchTask = watchTask;
+exports.cssProduction = cssProduction;
+
+/******************************************************
+ * COMPOUND TASKS
+ ******************************************************/
+
+exports.develop = parallel(
+  css,
+  html,
+  scripts,
+  typeface,
+  favicon,
+  images,
+  imageCopy,
+  watchTask
+);
+exports.production = parallel(
+  cssProduction,
+  htmlProduction,
+  scriptsProduction,
+  imgProduction,
+  typefaceProduction,
+  faviconProduction
+);
