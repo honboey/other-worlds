4294d2644dc11a8b356fc784e1065ef0abb847c8
Hon Boey <hon@dialogicstudios.com.au>
2020-06-22 12:40:55 +1000

    Started styling homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@ module.exports = {
   purge: [],
   theme: {
     extend: {
+      colors: {
+        ow: {
+          cream: "#eeece0",
+        },
+      },
       fontFamily: {
         yantramanav: ["Yantramanav", "sans-serif"],
         ogg: ["Ogg", "serif"],
